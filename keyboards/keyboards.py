4b80343421dfc02b9ba4ff770fa23f7dc003eb66
Максим Сафронов <maksimsafronov@MacBook-Air-Maksim.local>
2026-06-28 14:11:22 +0300

    fix keyboard update logic

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def get_keyboard(signs):
     keyboard = [
         [InlineKeyboardButton(text=sign.capitalize(), callback_data=sign)]
-        for sign in signs()
+        for sign in signs
     ]
     keyboard.append([InlineKeyboardButton(text="🔄 Обновить", callback_data="update")])
     return InlineKeyboardMarkup(inline_keyboard=keyboard)
