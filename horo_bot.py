e40269674be3f2989b2be787777f80677712f1d7
Максим Сафронов <maksimsafronov@MacBook-Air-Maksim.local>
2026-06-28 13:02:01 +0300

    add keep alive server for render

--- horo_bot.py
+++ horo_bot.py
@@ -1,5 +1,7 @@
 import asyncio
 import os
+from aiohttp import web
+import threading
 from keyboards.keyboards import get_keyboard
 from handlers.callback import router as callback_router
 from db import get_all_signs
@@ -17,13 +19,20 @@ dp = Dispatcher(storage=MemoryStorage())
 dp.include_router(callback_router)
 @dp.message(Command('start'))
 async def start(message: types.Message):
-    signs = get_all_signs
+    signs = get_all_signs()
     if not signs:
         await message.answer('Нет данных в базе')
         return
     keyboard = get_keyboard(signs)
     await message.answer('Выберите знак зодиака:', reply_markup=keyboard)
+async def handle(request):
+    return web.Response(text="Bot is alive")
+def run_web():
+    app = web.Application()
+    app.router.add_get("/", handle)
+    web.run_app(app, port=8080)
 async def main():
     await dp.start_polling(bot)
 if __name__ == '__main__':
+    threading.Thread(target=run_web, daemon=True).start()
     asyncio.run(main())
